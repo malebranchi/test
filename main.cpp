0086f7a9f04cdebb8091afc7b1150a14a14aabdb
Pawel Korotko <pawelK1@verifone.com>
2015-07-02 11:41:09 +0200

    init procjet version

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "libber-tlv\vxtypes.h"
+#include "libber-tlv\ber-tlv.h"
+#include "liblog\dlogger.h"
+
+int main(int argc, char *argv[])
+{
+	printf("hello\n");
+
+	return 0;
+}
